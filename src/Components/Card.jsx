51aaf951cf1bea18848ebb1bfc6c0ef997186a3b
DDKS <77142390+dhs26206@users.noreply.github.com>
2024-11-08 20:25:09 +0530

    Update Card.jsx
    Trying Something

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,7 +5,7 @@ const Card=({name}) => {
         <div className="m-8 w-[250px] bg-green-900 rounded-lg h-[300px] text-center ">
             <img className="h-4/5 border-b-2 border-black w-auto" alt={`${name}`} src={`https://robohash.org/${name}.png`}  />
             <h2 className="animate_animated animate__bounceIn text-yellow-300">{name}</h2>
-            <p className="text-white">{name}@outlook.com</p>
+            <p className="text-white">{name}@outlook.in</p>
         </div>
     );
 }
